06e19a90022595b45076cf31ed6233d5ee4d01b5
Atinuke <aodeshola@yahoo.com>
2024-01-26 19:17:08 +0100

    Save

---
+++ app/src/main/java/com/atinuke/calculator/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.atinuke.calculator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Yellow20 = Color(0xFF9C27B0)
+val ArmyGreen30 = Color(0xFF5A695A)
+val MediumGary = Color(0xF71A1919)
